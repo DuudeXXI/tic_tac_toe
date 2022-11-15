fc681859e690f2670b04e8aa083b8d2bc3f304a3
DuudeXXI <buslavicius.vaidas@gmail.com>
2022-11-15 11:17:06 +0200

    LocalStorage system working

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -3,7 +3,7 @@
 const Square = ({ value, chooseSquare}) => {
 
     return ( 
-        <div className="square" onClick={chooseSquare}>
+        <div className={`square ${value ? 'bg-salmon text-white' : ''}`} onClick={chooseSquare}>
             <span>{value}</span>
         </div>
      );
